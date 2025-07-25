0c6e308a15ad95405d31d1d3430d8b38feb24e9b
vys1n <kavysingh04@gmail.com>
2025-07-26 01:43:02 +0530

    Add supporting files and configuration

---
+++ build/CMakeFiles/http-server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/http-server.dir/src/main.c.o"
+  "CMakeFiles/http-server.dir/src/main.c.o.d"
+  "http-server"
+  "http-server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/http-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
